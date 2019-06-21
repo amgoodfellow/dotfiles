dd65c45d66f67406593e348ea4c4778e9d89195c
Aaron Goodfellow <agoodfellow@routeone.com>
2019-06-20 20:14:55 -0400

    Fun little functions

---
+++ custom-functions.el
@@ -0,0 +1,27 @@
+(defun my/add-junit-test (name type actual expected)
+  "Interactively adds junit test for given feature"
+  (interactive "sTest Name: \nsAssert Type: \nsActual: \nsExpected: \n")
+  (insert (message "@Test\nprivate void %s(){\n\tassert%s(%s, %s);\n}" name type actual expected)))
+
+(defun my/java-wrap-in-try (e)
+  (interactive "sException: \n")
+  (insert (concat "try {\n"
+  (filter-buffer-substring (region-beginning)(region-end))
+  "\} catch("e"Exception e) {\n\n}") (delete-region (region-beginning) (region-end))))
+
+(defun my/delimiter-switcher (str current desired)
+  "Takes a list of values and adds a delimiter to them"
+  (interactive "sString Value: \nsCurrent Delimiter: \nsDesired Delimiter: \n")
+  (insert (replace-regexp-in-string current desired str)))
+
+(defun my/markdown-codify (lang)
+  "Yank a region of code and wrap it in markdown code format"
+  (interactive "sLanguage: \n")
+  (kill-new (concat "```" lang "\n"
+   (filter-buffer-substring (region-beginning)(region-end))
+   "\n```")))
+
+(defun my/javadoc-comment (param return)
+  "Create a simple Javadoc comment"
+  (interactive "sParam: \nsReturn: \n")
+  (insert (concat "/**\n*\n*\n*\n* @param " param "\n* @return " return "\n*/")))
