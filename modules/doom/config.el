bf9956a396d16d4112450a9fba483adef69519f3
Aaron Goodfellow <amgoodfellow@protonmail.com>
2025-08-24 14:53:24 -0400

    remove some more comments

--- modules/doom/config.el
+++ modules/doom/config.el
@@ -1,47 +1,14 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
-
-
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
 (setq user-full-name "Aaron Goodfellow"
       user-mail-address "amgoodfellow@protonmail.com")
 
-;; Doom exposes five (optional) variables for controlling fonts in Doom:
-;;
-;; - `doom-font' -- the primary font to use
-;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
-;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;; - `doom-unicode-font' -- for unicode glyphs
-;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
-;;
-;; See 'C-h v doom-font' for documentation and more examples of what they
-;; accept. For example:
-;;
-;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
-;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
-;;
-;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
-;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
-;; refresh your font settings. If Emacs still can't find your font, it likely
-;; wasn't installed correctly. Font issues are rarely Doom issues!
-                                        ;(setq doom-font (font-spec :family "Fira Code" :size 21 ))
 (setq doom-font (font-spec :family "Fira Code" :size 21))
-
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
 (setq doom-theme 'doom-gruvbox)
-
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type t)
 
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/Documents/org")
 (setq org-roam-directory "~/Documents/org/roam")
 
@@ -73,14 +40,6 @@
 ;; This will open documentation for it, including demos of how they are used.
 ;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
 ;; etc).
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.
-
-;; Load custom functions
-                                        ;(load! "~/.doom/custom-functions.el")
-
-                                        ;(dirvish-override-dired-mode)
 
 (after! evil
   (setq evil-escape-key-sequence "jj")
@@ -99,9 +58,7 @@
     (setf (alist-get 'gleam apheleia-formatters) '("gleam" "format" "--stdin"))))
 
 (after! treesit
-  (add-to-list 'auto-mode-alist '("\\.gleam$" . gleam-ts-mode))
-  )
-
+  (add-to-list 'auto-mode-alist '("\\.gleam$" . gleam-ts-mode)))
 
 (after! eglot
   :config
